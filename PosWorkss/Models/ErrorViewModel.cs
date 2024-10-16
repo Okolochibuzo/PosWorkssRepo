66078c2dd78fdc93d09b7d25fbe56e002db3a551
Okolochibuzo <okolovalentine25@gmail.com>
2024-10-16 14:37:55 +0100

    Add project files.

---
+++ PosWorkss/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PosWorkss.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
